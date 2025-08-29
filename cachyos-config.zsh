f4547cfb409b7120c159a925200e07e9bee5f971
Yves Peissard <ypeissard@gmail.com>
2025-08-29 15:38:33 +0200

    Updated config files

--- cachyos-config.zsh
+++ cachyos-config.zsh
@@ -1,3 +1,5 @@
+# Put this file inside /usr/share/cachyos-zsh-config
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
